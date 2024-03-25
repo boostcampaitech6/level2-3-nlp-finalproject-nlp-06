ec3e084ebde2dce395f762bf308c154e66ff1de2
sujeong <jo9392@naver.com>
2024-03-25 23:33:58 +0900

    add dialog short case

--- remind.py
+++ remind.py
@@ -92,44 +92,47 @@ def make_remind(bot_summary_template, comment_template, dialog_summary, user_nam
     solar_chain = LLMChain(prompt=prompt, llm=solar)
     last_comment = solar_chain.run({'context_list' : bot_summary, 'user_name':user_name})
 
-    remind = bot_summary + '\n 루나의 한마디 : ' + last_comment
+    remind = bot_summary + '\n 지우의 한마디 : ' + last_comment
     return remind
 
 
 if __name__ == "__main__":
     MODEL_PATH = "./"
+    user_name = '수정'
     download_backbone_model(MODEL_PATH=MODEL_PATH)
     stop_words = ['보고서:', '회고:', '#', '[System command]', '[Example Prompt]', '\n\n']
-    dialog = read_dialog("./dialog/dialog_solar_jiu/dialog_history_5.txt")
-    dialog_summary = summarize(dialog, chunk_size=4)
+    dialog = read_dialog("./dialog/dialog_solar_jiu/dialog_history_short.txt")
+    if len(dialog) <= 5:
+        remind = f"지우는 {user_name}과 조금 더 이야기 하고 싶나봐요!"
+    else:
+        dialog_summary = summarize(dialog, chunk_size=4)
+        bot_summary_template = '''보고서는 {user_name}의 하루를 기록한 내용이야. 다음 지침과 예시를 참고해서 회고를 작성해줘.
+    [System command]
+    1. {user_name}의 하루를 지우라는 친구가 평가하는 내용을 작성해줘.
+    2. 친구가 {user_name}에게 말해주는 것처럼 친근한 말투로 써줘.
+    3. 모든 문장을 과거형으로 작성해줘.
 
-    bot_summary_template = '''보고서는 {user_name}의 하루를 기록한 내용이야. 다음 지침과 예시를 참고해서 회고를 작성해줘.
-[System command]
-1. {user_name}의 하루를 루나라는 친구가 평가하는 내용을 작성해줘.
-2. 친구가 {user_name}에게 말해주는 것처럼 친근한 말투로 써줘.
-3. 모든 문장을 과거형으로 작성해줘.
+    [Example Prompt]
+    보고서: 고등학교 친구인 예원이를 만나서 카페를 다녀왔다고 한다.
+    집에 돌아와서는 너무 늦게 들어왔다는 이유로 엄마와 다퉜다고 한다.
+    기분이 좋지 않았는데 대학 과제를 해야했기 때문에 열심히 다 마치고 잤다고 한다.
+    회고: {user_name}아! 너는 오늘 고등학교 친구 예원이랑 만나서 카페를 다녀왔었네. 재미있었어? {user_name}는 집에 늦게 들어왔다고 엄마와 다퉜었지. 속상했겠다.. 그리고 기분이 좋지 않았는데도 대학 과제를 마무리하고 잤구나. {user_name}는 대단한 것 같아!
 
-[Example Prompt]
-보고서: 고등학교 친구인 예원이를 만나서 카페를 다녀왔다고 한다.
-집에 돌아와서는 너무 늦게 들어왔다는 이유로 엄마와 다퉜다고 한다.
-기분이 좋지 않았는데 대학 과제를 해야했기 때문에 열심히 다 마치고 잤다고 한다.
-회고: {user_name}아! 너는 오늘 고등학교 친구 예원이랑 만나서 카페를 다녀왔었네. 재미있었어? {user_name}는 집에 늦게 들어왔다고 엄마와 다퉜었지. 속상했겠다.. 그리고 기분이 좋지 않았는데도 대학 과제를 마무리하고 잤구나. {user_name}는 대단한 것 같아!
+    보고서: {context_list}
+    회고:'''
+        
+        comment_template = '''주어진 회고는 지우라는 친구가 {user_name}의 하루를 기록한 내용이야. 다음 예시처럼 지우의 한마디를 작성해줘.
+    [System command]
+    1. {user_name}의 하루가 감정적으로 즐거웠는지, 슬펐는지, 어땠는지 언급해줘.
+    2. 항상 응원하는 친구가 {user_name}에게 응원하거나, 조언해주는 문장을 작성해줘.
 
-보고서: {context_list}
-회고:'''
-    
-    comment_template = '''주어진 회고는 루나라는 친구가 {user_name}의 하루를 기록한 내용이야. 다음 예시처럼 루나의 한마디를 작성해줘.
-[System command]
-1. {user_name}의 하루가 감정적으로 즐거웠는지, 슬펐는지, 어땠는지 언급해줘.
-2. 항상 응원하는 친구가 {user_name}에게 응원하거나, 조언해주는 문장을 작성해줘.
+    [Example Prompt]
+    회고: {user_name}아! 너는 오늘 고등학교 친구 예원이랑 만나서 카페를 다녀왔었네. 재미있었어? {user_name}는 집에 늦게 들어왔다고 엄마와 다퉜었지. 속상했겠다.. 그리고 기분이 좋지 않았는데도 대학 과제를 마무리하고 잤구나. {user_name}는 대단한 것 같아!
+    지우의 한마디: 오늘은 기분이 좋은 일도, 나쁜 일도 있었네. {user_name}가 항상 행복했으면 좋겠다!
 
-[Example Prompt]
-회고: {user_name}아! 너는 오늘 고등학교 친구 예원이랑 만나서 카페를 다녀왔었네. 재미있었어? {user_name}는 집에 늦게 들어왔다고 엄마와 다퉜었지. 속상했겠다.. 그리고 기분이 좋지 않았는데도 대학 과제를 마무리하고 잤구나. {user_name}는 대단한 것 같아!
-루나의 한마디: 오늘은 기분이 좋은 일도, 나쁜 일도 있었네. {user_name}가 항상 행복했으면 좋겠다!
+    회고:{context_list}
+    지우의 한마디:'''
 
-회고:{context_list}
-루나의 한마디:'''
-
-    remind = make_remind(bot_summary_template, comment_template, dialog_summary, user_name="수정", stop_words=stop_words)
-    print("summary\n", dialog_summary)
+        remind = make_remind(bot_summary_template, comment_template, dialog_summary, user_name=user_name, stop_words=stop_words)
+        print("summary\n", dialog_summary)
     print("remind\n",remind)
